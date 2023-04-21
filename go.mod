529f7e245f7fb9da0cc2e234ee21866f57869ded
mmich-pl <m.michal.parzych@gmail.com>
2023-04-21 16:34:28 +0200

    create mocks for Battleship Client

--- go.mod
+++ go.mod
@@ -14,5 +14,6 @@ require (
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/app/board_test.go
+++ internal/app/board_test.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"battleships/internal/battlehip_client"
+	gui "github.com/grupawp/warships-gui"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -31,6 +33,55 @@ func TestMapCoords(t *testing.T) {
 	}
 }
 
-func TestGenerateBoard(t *testing.T) {
+func TestSetUpBoard(t *testing.T) {
+	testScenario := struct {
+		testName string
+		client   func(t *testing.T) *battlehip_client.MockBattleshipClient
+	}{
+		testName: "Board Setup",
+		client: func(t *testing.T) *battlehip_client.MockBattleshipClient {
+			client := battlehip_client.NewMockBattleshipClient(t)
+			client.EXPECT().Board(BoardEndpoint).Return([]string{
+				"A7", "A8", "A9", "C3", "D6", "D9", "D10", "E3", "F3", "F6",
+				"G1", "G3", "G9", "G10", "H1", "H5", "I1", "J1", "J4", "J5"}, nil)
+			return client
+		},
+	}
 
+	t.Run(testScenario.testName, func(t *testing.T) {
+		client := testScenario.client(t)
+		response, _ := client.Board(BoardEndpoint)
+		expectedOpponent := [10][10]gui.State{}
+		expectedPlayer := [10][10]gui.State{}
+		for i := 0; i < len(expectedOpponent); i++ {
+			expectedOpponent[i] = [10]gui.State{}
+			expectedPlayer[i] = [10]gui.State{}
+		}
+
+		expectedPlayer[9][4] = gui.Ship
+		expectedPlayer[0][7] = gui.Ship
+		expectedPlayer[6][9] = gui.Ship
+		expectedPlayer[3][5] = gui.Ship
+		expectedPlayer[7][4] = gui.Ship
+		expectedPlayer[9][3] = gui.Ship
+		expectedPlayer[8][0] = gui.Ship
+		expectedPlayer[3][8] = gui.Ship
+		expectedPlayer[3][9] = gui.Ship
+		expectedPlayer[2][2] = gui.Ship
+		expectedPlayer[0][6] = gui.Ship
+		expectedPlayer[4][2] = gui.Ship
+		expectedPlayer[5][2] = gui.Ship
+		expectedPlayer[9][0] = gui.Ship
+		expectedPlayer[5][5] = gui.Ship
+		expectedPlayer[6][0] = gui.Ship
+		expectedPlayer[6][2] = gui.Ship
+		expectedPlayer[6][8] = gui.Ship
+		expectedPlayer[7][0] = gui.Ship
+		expectedPlayer[0][8] = gui.Ship
+
+		player, opponent, _ := setUpBoardsState(response)
+
+		assert.Equalf(t, *player, expectedPlayer, "Expected to be the same")
+		assert.Equalf(t, *opponent, expectedOpponent, "Expected to be the same")
+	})
 }
