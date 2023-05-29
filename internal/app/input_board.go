718c32b0ebb6de128a3a87f05febb110e7b4456a
mmich-pl <m.michal.parzych@gmail.com>
2023-05-29 20:46:38 +0200

    dynamic change amount of ships while placing own fleet

--- internal/app/input_board.go
+++ internal/app/input_board.go
@@ -1,6 +1,7 @@
-package board_utils
+package app
 
 import (
+	"battleships/internal/app/board_utils"
 	"battleships/internal/utils"
 	"context"
 	"fmt"
@@ -8,8 +9,13 @@ import (
 	"time"
 )
 
+var (
+	ui          = gui.NewGUI(false)
+	playerFleet = make(map[int]int)
+)
+
 func getPlayerFleet(stop chan struct{}, states [10][10]gui.State, board gui.Board, txt gui.Text, getPlayerFleet chan<- []string) {
-	playerFleet := make([]string, 0)
+	playerFleetCoords := make([]string, 0)
 
 	remove := func(s []string, element string) []string {
 		var index int
@@ -19,12 +25,12 @@ func getPlayerFleet(stop chan struct{}, states [10][10]gui.State, board gui.Boar
 			}
 		}
 		return append(s[:index], s[index+1:]...)
-
 	}
+
 	for {
 		select {
 		case <-stop:
-			getPlayerFleet <- playerFleet
+			getPlayerFleet <- playerFleetCoords
 			return
 		default:
 			coords := board.Listen(context.TODO())
@@ -32,39 +38,48 @@ func getPlayerFleet(stop chan struct{}, states [10][10]gui.State, board gui.Boar
 			switch s := &states[x][y]; *s {
 			case gui.Ship:
 				*s = gui.Empty
-				playerFleet = remove(playerFleet, coords)
+				playerFleetCoords = remove(playerFleetCoords, coords)
 				txt.SetText(fmt.Sprintf("Ship remove from %s", coords))
 			default:
 				*s = gui.Ship
-				playerFleet = append(playerFleet, coords)
-				txt.SetText(fmt.Sprintf("Ship set as %s", coords))
+				playerFleetCoords = append(playerFleetCoords, coords)
+				txt.SetText(fmt.Sprintf("Ship set at %s (%d)", coords, len(coords)))
 			}
+			updatePlayerFleet(playerFleetCoords)
 			board.SetStates(states)
 
-			if len(playerFleet) >= 20 {
+			if len(playerFleetCoords) == 20 {
 				board.SetStates(states)
 				txt.SetText("All fleet set!")
 				time.Sleep(time.Second * 2)
-				getPlayerFleet <- playerFleet
+				getPlayerFleet <- playerFleetCoords
 				return
 			}
 		}
 	}
 }
 
-func printFleetInstruction(ui gui.GUI) {
-	title := gui.NewText(60, 10, "Your fleet should look like this:", nil)
+func updatePlayerFleet(playerFleetCoords []string) {
+	b, _ := board_utils.MapCoordToBoard(playerFleetCoords)
+	blobs := board_utils.ConnectedComponentLabeling(b)
+	occurrences := board_utils.CountOccurrences(blobs)
+	for k, _ := range playerFleet {
+		playerFleet[k] = 0
+	}
+	for _, num := range occurrences {
+		playerFleet[num]++
+	}
+	printFleetInstruction()
+}
 
-	battleshipInfo := gui.NewText(60, 11, "1x Battleship  (4 tiles)", nil)
-	cruiserInfo := gui.NewText(60, 12, "2x Cruiser     (3 tiles)", nil)
-	destroyerInfo := gui.NewText(60, 13, "3x Destroyer   (2 tiles)", nil)
-	submarineInfo := gui.NewText(60, 14, "4x Submarine   (1 tile)", nil)
+func printFleetInstruction() {
+	header := gui.NewText(60, 10, fmt.Sprintf("%12s |\t%6s |\t%16s ", "Ship", "Size", "Can be placed"), nil)
+	ui.Draw(header)
 
-	ui.Draw(title)
-	ui.Draw(battleshipInfo)
-	ui.Draw(cruiserInfo)
-	ui.Draw(destroyerInfo)
-	ui.Draw(submarineInfo)
+	for k, v := range board_utils.ShipQuantities {
+		t := gui.NewText(60, 10+k, fmt.Sprintf("%12s |\t%6d |\t%16d ", ShipNames[k-1], k, v-playerFleet[k]), nil)
+		ui.Draw(t)
+	}
 }
 
 func initBaseState() [10][10]gui.State {
@@ -76,16 +91,19 @@ func initBaseState() [10][10]gui.State {
 }
 
 func RenderInputBoard() []string {
-	var playerFleet []string
+	for k, _ := range board_utils.ShipQuantities {
+		playerFleet[k] = 0
+	}
+	var playerFleetCoords []string
 	board := gui.NewBoard(1, 1, nil)
-	ui := gui.NewGUI(false)
+
 	states := initBaseState()
 	board.SetStates(states)
 	ui.Draw(board)
 
 	txt := gui.NewText(60, 5, "Press on any coordinate to set a ship.", nil)
 	ui.Draw(txt)
-	printFleetInstruction(*ui)
+	printFleetInstruction()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	stop := make(chan struct{})
@@ -96,8 +114,8 @@ func RenderInputBoard() []string {
 		var cause string
 		for !validBoard {
 			go getPlayerFleet(stop, states, *board, *txt, fleetChannel)
-			playerFleet = <-fleetChannel
-			validBoard, cause = ValidateShipPlacement(playerFleet)
+			playerFleetCoords = <-fleetChannel
+			validBoard, cause = board_utils.ValidateShipPlacement(playerFleetCoords)
 			txt.SetText(cause)
 			states = initBaseState()
 			board.SetStates(states)
@@ -107,5 +125,5 @@ func RenderInputBoard() []string {
 	}()
 	ui.Start(ctx, nil)
 
-	return playerFleet
+	return playerFleetCoords
 }
